3cfd08792f854d96351acfc98abd33b948c3a51d
7hanatos13@gmail.com <7hanatos13@gmail.com@d2da2402-b21c-2a4d-16d0-0f995097abc6>
2011-12-25 12:44:35 +0000

    Encode từ khóa.

--- trunk/Bingle/src/java/util/test.java
+++ trunk/Bingle/src/java/util/test.java
@@ -4,7 +4,13 @@
  */
 package util;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,16 +22,30 @@ public class test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //ArrayList<Article> GPage1 = SearchEngine.SearchGoogle("flowers", 1, 10);
-        //ArrayList<Article> GPage2 = SearchEngine.SearchGoogle("flowers", 2);
-        //ArrayList<Article> BingArticles = SearchEngine.SearchBing("flowers", 1, 10);
-        //ArrayList<Article> BingLibraryArticles = SearchEngine.SearchBingLibrary("flowers", 1);
-        //ArrayList<Article> articles = SearchEngine.Search("flowers", 1, 5);
+        try {
+            // TODO code application logic here
+            //ArrayList<Article> GPage1 = SearchEngine.SearchGoogle("flowers", 1, 10);
+            //ArrayList<Article> GPage2 = SearchEngine.SearchGoogle("flowers", 2);
+            //ArrayList<Article> BingArticles = SearchEngine.SearchBing("flowers", 1, 10);
+            //ArrayList<Article> articles = SearchEngine.Search("flowers", 1, 5);
+            //ArrayList<Article> articles = SearchEngine.Search("flowers", 1, 5);
 
-        System.out.println("Hello World");
-
-        int a;
-        a = 10;
+            //System.out.println("Hello World");
+            //Create search string
+            String APIKey = "AIzaSyDvysnoSg7Xlw4sKcmtdKhsRx_EaD_59TM";
+            String CSEID = "006128248623655005956:_w9w403uat0";
+            int startIndex = (1 - 1) * 10 + 1;
+            String aKeyword = "flowers";
+            String urlTemplate = "https://www.googleapis.com/customsearch/v1?key="
+                    + APIKey + "&cx=" + CSEID + "&q=" + aKeyword + "&num=" + 3 + "&start=" + startIndex + "&alt=json";
+            //Connect
+            //URL url = new URL(urlTemplate);
+            //URLConnection connection = url.openConnection();
+            String abc = URLEncoder.encode("giá trị", "UTF-8");
+            int a;
+            a = 10;
+        } catch (Exception ex) {
+            Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
