8aaa41e3c73baf4963894568ec13a72f57491a25
abcxyz2357@gmail.com <abcxyz2357@gmail.com@d2da2402-b21c-2a4d-16d0-0f995097abc6>
2011-12-25 14:48:49 +0000

    Đã sửa lại vị trí lưu file log.

--- trunk/Eclipse/src/controller/indexController.java
+++ trunk/Eclipse/src/controller/indexController.java
@@ -12,6 +12,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.apache.log4j.Logger;
+
 import util.Article;
 import util.SearchEngine;
 
